ddc5ed3815ecf56806153af3f9d4f43de3e7b637
phil bridge <pbridge@qa.com>
2022-04-27 07:54:38 +0100

    adding src

---
+++ src/Primitives.java
@@ -0,0 +1,17 @@
+public class Primitives {
+    public static void main (String[] args){
+        //Primitives - Int Float Long Byte 
+
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+
+
+    }
+
+}
