3010db2784676eb9a6391352d83d24117ec32e5d
phil bridge <pbridge@qa.com>
2022-05-03 07:51:23 +0100

    clean up

--- src/FloatsDoubles.java
+++ src/FloatsDoubles.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class FloatsDoubles {
+public class FloatsDoubles {
+    public static void main(String[] args){
+        /*
+        * This is a simple exercise, look at the changes in the output if we don't declare types in the
+        * operations (float)
+         */
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= " + myDoubleValue);
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+    }
 }
